447df8104df43b04648058f333f21c0bf8236ff9
geertsky <geert@verweggistan.eu>
2024-12-23 20:46:01 +0000

    Build against sysroot_linux-64

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,11 +3,15 @@
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/libtool/build-aux/config.* support/
 
+export CPPFLAGS="--sysroot=$CONDA_BUILD_SYSROOT -I$CONDA_BUILD_SYSROOT/include"
+export LDFLAGS="--sysroot=$CONDA_BUILD_SYSROOT -L$CONDA_BUILD_SYSROOT/lib64"
+
+./configure 
 ./configure --prefix=${PREFIX}  \
             --build=${BUILD}    \
             --host=${HOST}      \
             --disable-static    \
-            --with-curses       \
+            --with-curses     CPPFLAGS="$CPPFLAGS" LDFLAGS="$LDFLAGS"  \
             || { cat config.log; exit 1; }
 make SHLIB_LIBS="$(pkg-config --libs ncurses)" -j${CPU_COUNT} ${VERBOSE_AT}
 make install
